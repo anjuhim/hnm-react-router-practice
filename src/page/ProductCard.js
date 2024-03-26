d42edc52a0cd894c8c777a895aa199fef9442793
juhim <zzonzzon84@nate.com>
2024-03-27 01:08:11 +0900

    #1. 쇼핑몰 웹사이트 만들기

---
+++ src/page/ProductCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img
+        src="https://lp2.hm.com/hmgoepprod?set=source[/00/a7/00a75fe39e6cb81da8cdccc3f52e294f77c94254.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]"
+        alt="리넨 블렌드 재킷"
+      />
+      <div>리넨 블렌드 재킷</div>
+      <div>₩99,900</div>
+      <div>신제품</div>
+    </div>
+  );
+};
+
+export default ProductCard;
