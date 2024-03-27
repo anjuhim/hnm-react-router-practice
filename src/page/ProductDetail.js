9be4bb15663256429d426b15fee72baf6fad5b03
juhim <zzonzzon84@nate.com>
2024-03-28 02:08:46 +0900

    로딩 추가

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Row, Stack } from 'react-bootstrap';
-import { Form, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import FormSelect from 'react-bootstrap/FormSelect';
+import { SpinnerCircular } from 'spinners-react';
 
 const ProductDetail = () => {
   const [item, setItem] = useState({});
@@ -25,27 +26,33 @@ const ProductDetail = () => {
   return (
     <Container>
       <Row className="justify-content-md-center">
-        <Col>
-          <img src={item?.img} alt={item?.title} />
-        </Col>
-        <Col>
-          <Stack gap={2}>
-            <div>{item?.title}</div>
-            <div>₩{item?.price.toLocaleString('ko-KR')}</div>
-            <div>{item?.choice ? 'concious choice' : ''}</div>
-            <div>
-              <FormSelect>
-                <option>사이즈 선택</option>
-                {item.size?.map((size, index) => (
-                  <option key={index} value={size}>
-                    {size}
-                  </option>
-                ))}
-              </FormSelect>
-            </div>
-            <Button variant="dark">추가</Button>
-          </Stack>
-        </Col>
+        {loading ? (
+          <SpinnerCircular enabled={loading} size={100} />
+        ) : (
+          <>
+            <Col>
+              <img src={item?.img} alt={item?.title} />
+            </Col>
+            <Col>
+              <Stack gap={2}>
+                <div>{item?.title}</div>
+                <div>₩{item?.price.toLocaleString('ko-KR')}</div>
+                <div>{item?.choice ? 'concious choice' : ''}</div>
+                <div>
+                  <FormSelect>
+                    <option>사이즈 선택</option>
+                    {item.size?.map((size, index) => (
+                      <option key={index} value={size}>
+                        {size}
+                      </option>
+                    ))}
+                  </FormSelect>
+                </div>
+                <Button variant="dark">추가</Button>
+              </Stack>
+            </Col>
+          </>
+        )}
       </Row>
     </Container>
   );
