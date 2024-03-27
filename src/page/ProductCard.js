98913a073d4f1645fe59a0e927c4f56d9cb7af24
juhim <zzonzzon84@nate.com>
2024-03-28 02:01:24 +0900

    card 에서 호출 url변경

--- src/page/ProductCard.js
+++ src/page/ProductCard.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const ProductCard = ({ item }) => {
   const navigate = useNavigate();
   const showDetail = () => {
-    navigate(`/product/${item?.id}`);
+    navigate(`/anjuhim/hnm-react-router-practice/product/${item?.id}`);
   };
   return (
     <div className="product-card" onClick={showDetail}>
